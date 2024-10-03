3b7b49b2e08a98aa9c6cb92e4201e372318b2471
ArthurHtbk <arthur.heurtebise@gmail.com>
2024-10-03 14:26:02 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Défilement fluide lors du clic sur un lien de navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
